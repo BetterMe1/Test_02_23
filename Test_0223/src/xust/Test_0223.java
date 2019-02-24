bb3035d21281f9a7847ce5ea7cbae29a57259a90
BetterMe1 <1754217281@qq.com>
2019-02-24 22:36:53 +0800

    更新代码

--- Test_0223/src/xust/Test_0223.java
+++ Test_0223/src/xust/Test_0223.java
@@ -1,61 +1,51 @@
 package xust;
 
+
 /*
-819. 最常见的单词
- 给定一个段落 (paragraph) 和一个禁用单词列表 (banned)。返回出现次数最多，同时不在禁用列表中的单词。题目保证至少有一个词不在禁用列表中，而且答案唯一。
-禁用列表中的单词用小写字母表示，不含标点符号。段落中的单词不区分大小写。答案都是小写字母。
- 
-示例：
-输入: 
-paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
-banned = ["hit"]
-输出: "ball"
-解释: 
-"hit" 出现了3次，但它是一个禁用的单词。
-"ball" 出现了2次 (同时没有其他单词出现2次)，所以它是段落里出现次数最多的，且不在禁用列表中的单词。 
-注意，所有这些单词在段落里不区分大小写，标点符号需要忽略（即使是紧挨着单词也忽略， 比如 "ball,"）， 
-"hit"不是最终的答案，虽然它出现次数更多，但它在禁用单词列表中。
- 
-说明：
-1 <= 段落长度 <= 1000.
-1 <= 禁用单词个数 <= 100.
-1 <= 禁用单词长度 <= 10.
-答案是唯一的, 且都是小写字母 (即使在 paragraph 里是大写的，即使是一些特定的名词，答案都是小写的。)
-paragraph 只包含字母、空格和下列标点符号!?',;.
-不存在没有连字符或者带有连字符的单词。
-单词里只包含字母，不会出现省略号或者其他标点符号。
+821. 字符的最短距离
+ 给定一个字符串 S 和一个字符 C。返回一个代表字符串 S 中每个字符到字符串 S 中的字符 C 的最短距离的数组。
+
+示例 1:
+输入: S = "loveleetcode", C = 'e'
+输出: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+说明:
+字符串 S 的长度范围为 [1, 10000]。
+C 是一个单字符，且保证是字符串 S 里的字符。
+S 和 C 中的所有字母均为小写字母。
+ */
+/*
+ * 分析：遍历一遍S，找出字符C在S中的索引，将索引存入数组Cindex中，再次遍历字符串S，将每一个字符索引与Cindex中索引的最小差绝对值存入数组res中，返回res。
+ * 
  */
 public class Test_0223 {
 	public static void main(String[] args) {
 		Solution So = new Solution();
-		String paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
-		String[] banned = {""};
-		So.mostCommonWord(paragraph, banned);
+		String S = "loveleetcode";
+		char C = 'e';
+		int[] res = So.shortestToChar(S, C);
+		for(int i=0; i<res.length; i++){
+			System.out.print(res[i]+" ");
+		}
 	}
 }
 class Solution {
-    public void mostCommonWord(String paragraph, String[] banned) {
-    	paragraph = paragraph.toLowerCase();
-    	paragraph = paragraph.replace(",", " ").replace(".", " ").replace("?", " ").replace("'", " ").replace("!", " ").replace(";", " ");
-    	int len = 0;
-    	for(int i=0; i<paragraph.length(); i++){
-    		if(String.valueOf(paragraph.charAt(i)) == "")
-    			len++;
-    	}
-    	String[] word = new String[len];
-    	String temp = "";
-    	len = 0;
-    	for(int i=0; i<paragraph.length(); i++){
-    		if(String.valueOf(paragraph.charAt(i)) != "")
-    			temp += String.valueOf(paragraph.charAt(i));
-    		else{
-    			word[len]= temp;
-    			len++;
-    			temp = "";
-    		}	
-    	}
-    	for(int i=0; i<word.length; i++){
-    		System.out.println(word[i]);
-    	}
+    public int[] shortestToChar(String S, char C) {
+        int[] Cindex = new int [S.length()];
+        int[] res = new int [S.length()];
+        int index = 0;
+        for(int i=0; i<S.length(); i++){
+        	if(S.charAt(i)==C){
+        		Cindex[index] = i;
+        		index++;
+        	}
+        }
+        for(int i=0; i<S.length(); i++){
+        	int min = 2147483647;
+        	for(int j=0;j<index;j++){
+        		min = Math.min(Math.abs(Cindex[j] - i), min);
+        	}
+        	res[i] = min;
+        }
+        return res;
     }
 }
